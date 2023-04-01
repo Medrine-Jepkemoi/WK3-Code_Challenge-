439192bfa3d488f3eab8a2a36f1a2486eee3320d
Medrine-Jepkemoi <jepkemoimedrine@gmail.com>
2023-04-01 21:56:33 +0300

    lising all movies in the menu

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 document.addEventListener("DOMContentLoaded", (e) => {
-  renderOneFilm();
-});
+  const menu = document.querySelector("#movieMenu");
 
-//get request function
-function renderOneFilm() {
+  //function to render one film
+  function renderOneFilm(film) {
+    //list all movies in the menu
+    const list = document.querySelector("#films .filmItem");
+    list.textContent += film.title;
+  }
+
+  //get request function
   fetch("http://localhost:3000/films")
-    .then((res) => res.json)
-    .then((res) => console.log(res));
-}
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((film) => {
+        renderOneFilm(film);
+      });
+    });
+});
